12275f0282192b2685d113cc0e31e5328c0617ae
Никитосик <nik-ar98@mail.ru>
2023-07-26 22:09:11 +0500

    Test failures

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -1,2 +1,30 @@
-package PACKAGE_NAME;public class ProductManagerTest {
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import ru.netology.*;
+
+public class ProductManagerTest {
+    ProductRepository repo = new ProductRepository();
+    ProductManager manager = new ProductManager(repo);
+    Product item1 = new Smartphone(13, "Iphone11", 350, "Apple");
+    Product item2 = new Smartphone(16, "Iphone13Pro", 350, "Apple");
+    Product item3 = new Smartphone(1, "A50", 350, "Samsung");
+    Product item4 = new Book(153, "The Prize", 350, "Ergin");
+
+    @BeforeEach
+    public void setUp() {
+        manager.add(item1);
+        manager.add(item2);
+        manager.add(item3);
+        manager.add(item4);
+    }
+
+    @Test
+    public void shouldSerch1() {
+        Product[] expected = {item2};
+        Product[] actual = manager.serchBy("Iphone13Pro");
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
 }
--- src/test/java/ProductRepositoryTest.java
+++ src/test/java/ProductRepositoryTest.java
@@ -1,2 +1,28 @@
-package PACKAGE_NAME;public class ProductRepositoryTest {
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.netology.Book;
+import ru.netology.Product;
+import ru.netology.ProductRepository;
+import ru.netology.Smartphone;
+
+public class ProductRepositoryTest {
+
+    Product item1 = new Smartphone(13, "Iphone11", 350, "Apple");
+    Product item2 = new Smartphone(16, "Iphone13Pro", 350, "Apple");
+    Product item3 = new Smartphone(1, "A50", 350, "Samsung");
+    Product item4 = new Book(153, "The Prize", 350, "Ergin");
+
+    @Test
+    public void shouldSave() {
+        ProductRepository repo = new ProductRepository();
+        repo.save(item1);
+        repo.save(item2);
+        repo.save(item3);
+        repo.save(item4);
+        Product[] expected = {item1, item2, item3, item4};
+        Product[] actual = repo.getItems();
+
+        Assertions.assertArrayEquals(expected, actual);
+    }
+
 }
